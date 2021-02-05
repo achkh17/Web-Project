2f3c6fecfa1569614b1ed5cfe29b20baa3b7d1ba
achkh17 <achkh17@freeuni.edu.ge>
2021-02-05 14:36:00 +0400

    removed jquery usages

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,28 +1,27 @@
 function hamburgerMenuClicked(x) {
-	x.classList.toggle("change");
+    x.classList.toggle("change");
 
-	var header = document.getElementById("header");
-	var sticky = header.offsetTop;
-	var x = document.getElementById("navMenu");
+    var header = document.getElementById("header");
+    var sticky = header.offsetTop;
+    var x = document.getElementById("navMenu");
 
-	if (x.style.display === "block") {
-		x.style.display = "none";
-		header.classList.remove("sticky");
-	} else {
-		x.style.display = "block";
-		header.classList.add("sticky");
-	}
-
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        header.classList.remove("sticky");
+    } else {
+        x.style.display = "block";
+        header.classList.add("sticky");
+    }
 }
 
 
 function openSubmenu(y) {
-	var x = y.parentElement.children[1];
-	if (x.style.display === "block") {
-		x.style.display = "none";
-		y.parentElement.children[0].children[1].style.transform = "none";
-	} else {
-		x.style.display = "block";
-		y.parentElement.children[0].children[1].style.transform = "rotate(180deg)";
-	}	
+    var x = y.parentElement.children[1];
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.parentElement.children[0].children[1].style.transform = "none";
+    } else {
+        x.style.display = "block";
+        y.parentElement.children[0].children[1].style.transform = "rotate(180deg)";
+    }	
 }
